6ecd7efba1c475605ee9bd320bc110c9202e6453
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-01 01:40:08 +0000

    Homework 1

--- src/examples/01_data_types/04_char/main.cpp
+++ src/examples/01_data_types/04_char/main.cpp
@@ -1,5 +1,16 @@
+#include<iostream>
+#include "char.h"
+
+using std::cout;
+
+
 int main()
 {
+    char letter = 'A';
+
+    double ascii_value = get_char_ascii_value(letter);
+
+    cout<<"The ascii value for"<<letter<<" is " <<ascii_value;
 
     return 0;
 }
