c71e32584f573d944a17efab5336929bc3498a44
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-06 00:55:42 +0000

    intro boolean expressions

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -41,3 +41,55 @@ TEST_CASE("Test the get+char ascii value function", "char to int conversion")
 
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
 
+
+/*test boolean operators*/
+TEST_CASE("test the AND operator", "only T T returns T") 
+{
+	REQUIRE(get_and_result(3,5) == true);
+	REQUIRE(get_and_result(5,3) == false);
+	REQUIRE(get_and_result(3,1) == false);
+	REQUIRE(get_and_result(1,5) == false);
+	
+}
+
+
+TEST_CASE("test the OR operator", "only F F returns F") 
+{
+	REQUIRE(get_or_result(true,true) == true);
+	REQUIRE(get_or_result(true,false) == true);
+	REQUIRE(get_or_result(false,true) == true);
+	REQUIRE(get_or_result(false,false) == false);
+
+}
+
+
+TEST_CASE("test the negation operator", "negate values") 
+{
+	REQUIRE(get_bool_negation(true) == false);
+	REQUIRE(get_bool_negation(false) == true);
+
+}
+
+
+TEST_CASE("test the boolean operators order of precedence", "order of operations") 
+{
+	REQUIRE(get_order_of_precedence_result(true, true, true) == true);
+	REQUIRE(get_order_of_precedence_result(true, true, false) == false);
+
+}
+
+
+TEST_CASE("test a use case for boolean operators", "negate a result") 
+{
+	REQUIRE(is_letter_a_consonant('a') == false);
+	REQUIRE(is_letter_a_consonant('z') == true);
+
+}
+
+
+TEST_CASE("test a number in a range", "using OR") 
+{
+	REQUIRE(is_number_not_in_range(0,1,10) == true);
+	REQUIRE(is_number_not_in_range(5,1,10) == false);
+
+}
