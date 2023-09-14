fe3a8b2ae4093ef3e080c9783c0f4006264366e6
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-14 03:52:03 +0000

    Homework 3

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,21 @@
 //write include statements
+#include<iostream>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	auto grade = 0;
+	cout<<"Enter your grade: ";
+	cin>>grade;
+
+	auto result_if_else_if = get_letter_grade_using_if(grade);
+	cout<<"Using if else if, your grade is: "<<result_if_else_if<<"\n";
+
+	auto result_switch = get_letter_grade_using_switch(grade);
+	cout<<"Using Switch, your grade is: "<<result_switch<<"\n";
+
 	return 0;
 }
