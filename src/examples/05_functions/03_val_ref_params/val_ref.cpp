49c71509657e5a13b4f8de91c9b16b0a66663110
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-21 05:22:20 +0000

    function reference and const ref param

---
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -0,0 +1,16 @@
+#include "val_ref.h"
+
+using std::cout;
+
+void ref_param(int &num)
+{
+    num = 20;
+
+}
+
+
+void const_ref_param(const int &num)
+{
+    cout<<"const int &num is available in read only mode: "<<num<<"\n";
+    //num = 20; This statement causes a read-only reference error; can't run program
+}
--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "void.h"
+#include "val_ref.h"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -14,4 +16,14 @@ TEST_CASE("Test function value param")
 	value_param(num);
 
 	REQUIRE(num == 10);
+}
+
+TEST_CASE("Test function reference parameter")
+{
+	int num = 10;
+
+	ref_param(num);
+
+	REQUIRE(num == 20);
+	
 }
