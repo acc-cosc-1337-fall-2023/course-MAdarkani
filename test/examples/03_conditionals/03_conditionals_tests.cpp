c906ee5877d90d62ff36c7bffc33aafa0fedce85
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-21 02:56:59 +0000

    menu example w/ switch

--- test/examples/03_conditionals/03_conditionals_tests.cpp
+++ test/examples/03_conditionals/03_conditionals_tests.cpp
@@ -29,12 +29,3 @@ TEST_CASE("Test get generation function", "verification")
 }
 
 
-TEST_CASE("Test switch menu function", "verification") 
-{
-	REQUIRE(menu(0) == "Invalid Option");
-	REQUIRE(menu(1) == "Option 1");
-	REQUIRE(menu(2) == "Option 2");
-	REQUIRE(menu(3) == "Option 3");
-	REQUIRE(menu(4) == "Option 4");
-	REQUIRE(menu(5) == "Invalid Option");
-}
