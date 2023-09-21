813107582605723c8b4acb18d0f5e433c6948991
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-21 02:44:54 +0000

    use menu switch in main

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
 	
+	auto option = 0;
+	
+	cout<<"Enter a number: ";
+	cin>>option;
+
+	string result = menu(option);
+	cout<<result<<"\n";
 
 	return 0;
 }
