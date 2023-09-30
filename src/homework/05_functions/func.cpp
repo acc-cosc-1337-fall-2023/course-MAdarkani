2c523ba4c0b4a783e2ba7a27897462650ee0f5c1
MAdarkani <143145275+MAdarkani@users.noreply.github.com>
2023-09-30 06:55:28 +0000

    Homework 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,57 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+
+
+double get_gc_content(const std::string& dna)
+{
+    double gc_content = 0;
+
+    for (int i = 0; i < dna.size(); i++)
+    {
+        if(dna[i]=='C' || dna[i]=='G')
+            gc_content++;
+        
+    }
+
+    return gc_content/dna.size();
+    
+}
+
+
+
+std::string reverse_string (std::string dna)
+{
+    std::string r_string;
+
+    for (int i = dna.size()-1; i >= 0 ; i--)
+    {
+        r_string += dna[i];
+    }
+    
+    return r_string;
+
+}
+
+
+std::string get_dna_complement (std::string dna)
+{
+    std::string complement = reverse_string(dna);
+    for (int i = 0; i < complement.size(); i++)
+    {
+        if (complement[i] == 'A')
+            complement[i] = 'T';
+        else if (complement[i] == 'T')
+            complement[i] = 'A';
+        else if (complement[i] == 'G')
+            complement[i] = 'C';
+        else if (complement[i] == 'C')
+            complement[i] = 'G';
+
+    }
+    
+    return complement;
+
+}
+
+
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,65 @@
+#include "func.h"
+#include <iostream>
+
+
+using std::cout;
+using std::cin;
+using std::string;
+
+//write using statements
 
 int main() 
 {
+	int choice = 0;
+    
+    do
+    {
+        cout << "1-Get GC Content\n";
+        cout << "2-Get DNA Complement\n";
+        cout << "3-Exit\n";
+        cout << "Enter your choice: ";
 
+        cin >> choice;
+        std::string dna_input;
+
+        switch (choice)
+        {
+
+        case 1:
+            cout << "Enter your DNA: ";
+            cin >> dna_input;
+            cout << "Your GC Content is: " << get_gc_content(dna_input) <<"\n";
+            break;
+
+        
+        case 2:
+            cout << "Enter your DNA: ";
+            cin >> dna_input;
+            cout << "Your DNA Complement is: " << get_dna_complement(dna_input) <<"\n";
+            break;
+
+        
+
+        case 3:
+            char confirmation;
+            cout << "are you sure you want to exit? (y)/(n)\n";
+            cin >> confirmation;
+            if (confirmation == 'y' || confirmation == 'Y')
+            {
+                cout<<"Goodbye!\n";
+                return 0;
+            }
+            else
+				choice = -1;
+
+            break;
+    
+        
+        default:
+            cout << "Invalid choice. Please pick a number between 1-3.\n";
+            break;
+        }
+
+    } while (choice != 3);
 	return 0;
 }
